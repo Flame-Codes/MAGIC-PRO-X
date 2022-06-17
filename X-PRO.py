03ade2d5d3d85ccd6d76bedd8ea45298ff181b1c
FLAME NAIM <96010467+Naim75o@users.noreply.github.com>
2022-06-17 09:31:29 +0600

    Update X-PRO.py

--- X-PRO.py
+++ X-PRO.py
@@ -1,5 +1,30 @@
-import os, sys
+import os, platform
+ 
 try:
-    __import__("MAGIC").Subscraption()
-except Exception as e:
-    exit(str(e))
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        from X64 import Subscraption
+ 
+        Subscraption()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from X32 import Subscraption
+ 
+        Subscraption()
+ 
