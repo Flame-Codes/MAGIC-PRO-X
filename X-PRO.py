6f19d41ccaa79f48db9eab5a58b3d82c7ced41e5
FLAME NAIM <96010467+Naim75o@users.noreply.github.com>
2023-04-04 13:48:42 +0600

    Update X-PRO.py

--- X-PRO.py
+++ X-PRO.py
@@ -1,30 +1,7 @@
-import os, platform
- 
+import os, sys
 try:
+    __import__("pro").Main()
+except Exception as e:
+    exit(str(e))
  
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from X64 import Subscraption
- 
-        Subscraption()
- 
- 
- 
-elif bit == "32bit":
- 
-        from X32 import Subscraption
- 
-        Subscraption()
  
